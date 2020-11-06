8b6213a4b53731ec013feb2cbc1000b49334471f
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 13:39:27 +1300

    Updates

---
+++ precache-manifest.f9948be3a36fc3a35ab926555b03591e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06491109296c7b9dee0bb6aa0b8ca34b",
+    "url": "/React-App-Foreign-Currency-Converter/index.html"
+  },
+  {
+    "revision": "e3116526679b692254d7",
+    "url": "/React-App-Foreign-Currency-Converter/static/css/main.b5ebff49.chunk.css"
+  },
+  {
+    "revision": "19ff51700faddd061ccf",
+    "url": "/React-App-Foreign-Currency-Converter/static/js/2.1659b62d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-Foreign-Currency-Converter/static/js/2.1659b62d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3116526679b692254d7",
+    "url": "/React-App-Foreign-Currency-Converter/static/js/main.110c349e.chunk.js"
+  },
+  {
+    "revision": "d7fc703989f958dabbfd",
+    "url": "/React-App-Foreign-Currency-Converter/static/js/runtime-main.07683450.js"
+  }
+]);
